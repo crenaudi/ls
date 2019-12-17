4dc2a22a22e2223844fcffc6a6aa2c946c526b34
Camille RENAUDIN <crenaudi@e1r13p11.42.fr>
2019-12-18 00:36:30 +0100

    change order

--- src/main.c
+++ src/main.c
@@ -6,11 +6,20 @@
 /*   By: crenaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 13:08:43 by crenaudi          #+#    #+#             */
-/*   Updated: 2019/12/10 14:19:38 by crenaudi         ###   ########.fr       */
+/*   Updated: 2019/12/18 00:34:28 by crenaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
+
+static void		test_argv(char *wrong, struct stat buf)
+{
+	if (device_type(buf) != 102)
+		ft_putendl(wrong);
+	else
+		error(wrong, -1, NULL);
+	ft_putchar('\n');
+}
 
 static void		norme_main(t_env *e, char **av, int i)
 {
@@ -20,13 +29,17 @@ static void		norme_main(t_env *e, char **av, int i)
 	j = -1;
 	buf = NULL;
 	while (av[i] != NULL)
-		 ft_strcpy(e->curr[++j], av[i++]);
+		ft_strcpy(e->curr[++j], av[i++]);
 	buf = buf_tab(e, "./");
 	e->f_sort(buf, e, ln_tab(e->curr));
-	//mettre les dossiers non existant en premier
 	j++;
 	while (j--)
-		empiler(e, e->curr[j], ft_strdup("./"));
+	{
+		if (device_type(buf[j]) != 100)
+			test_argv(e->curr[j], buf[j]);
+		else
+			empiler(e, e->curr[j], ft_strdup("./"));
+	}
 	clean(e->curr);
 	clean_ptr((void **)(&buf));
 	run(depiler(e->pile), e);
