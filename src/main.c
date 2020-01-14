1cf30efb004648be6bbf3626bc336f301cf27499
Camille RENAUDIN <crenaudi@e1r13p4.42.fr>
2020-01-14 22:43:52 +0100

    leaks main a voir

--- src/main.c
+++ src/main.c
@@ -12,69 +12,69 @@
 
 #include "../includes/ft_ls.h"
 
-static void		check_wrong_av(char **wrong)
+static void		print_wrongORfile(char **str, int bool)
 {
 	int i;
 
-	i = 0;
-	if (wrong[i][0] != 0)
+	i = -1;
+	if (str[0][0] != 0)
 	{
-		sort_base(NULL, wrong, ln_tab(wrong));
-		while (wrong[i][0] != 0)
-				error(wrong[i++], -1, ' ');
-		clean(wrong);
+		sort_base(NULL, str, ln_tab(str));
+		while (str[++i][0] != 0)
+				(bool == 0) ? ft_putendl(str[i]) : error(str[i], -1, ' ');
+		clean(str);
 	}
-	clean_ptr((void **)(&wrong));
+	clean_ptr((void **)(&str));
 }
 
 void check_argv(char **argv, t_env *e, int i, int len)
 {
 	char		*tmp;
 	char		**wrong;
-	int			j;
-	int			k;
+	char 		**file;
+	int 		index[4] = {-1, -1, 0, 0};
 
-	j = -1;
-	k = 0;
 	wrong = init_file();
+	file = init_file();
 	if (!(e->buf = (struct stat *)malloc(sizeof(struct stat) * (len))))
 		return ;
 	while (++i < len)
 	{
-		tmp = ft_strjoin((argv[i][0] == '/') ? argv[i] : "./", argv[i]);
-		if (!(lstat(tmp, &e->buf[k])))
-			ft_strcpy(e->current[k++], argv[i]);
+		tmp = ft_strjoin((argv[i][0] == '/') ? NULL : "./", argv[i]);
+		if (!(stat(tmp, &e->buf[index[2]])))
+		{
+
+			if (device(e->buf[index[2]]) == 'd' || device(e->buf[index[2]]) == 'l')
+				ft_strcpy(e->current[index[3]++], argv[i]);
+			else
+				ft_strcpy(file[++index[0]], argv[i]);
+			index[2]++;
+		}
 		else
-			ft_strcpy(wrong[++j], argv[i]);
+			ft_strcpy(wrong[++index[1]], argv[i]);
 		ft_strdel(&tmp);
 	}
-	check_wrong_av(wrong);
+	print_wrongORfile(wrong, -1);
+	print_wrongORfile(file, 0);
 }
 
-int 	push_stack(t_env *e, char **argv, int start, int len)
+int 	push_stack(t_env *e, char **argv, int start, int end)
 {
+	int 	len;
 	int 	i;
 
-	i = -1;
-	check_argv(argv, e, start - 1, len);
-	e->f_sort(e->buf, e->current, ln_tab(e->current));
-	while (e->current[++i][0] != 0)
-	{
-			if (device(e->buf[i]) == 'l' && e->current[i][ft_strlen(e->current[i]) - 1] != '/')
-				print_lnk(e->buf[i], e->current[i], e->pile);
-			else if (e->current[i][0] != '-' && (device(e->buf[i]) == 'd' || device(e->buf[i]) == 'l'))
-			{
-				(e->current[i][0] == '/') ? push(e->pile, e->current[i], NULL)
-					: push(e->pile, argv[i], "./");
-			}
-			else
-				ft_putendl(e->current[i]);
-	}
+	i = 0;
+	check_argv(argv, e, start - 1, end);
+	len = ln_tab(e->current);
+	e->f_sort(e->buf, e->current, len);
+	while (i <= --len)
+		(e->current[len][0] == '/') ? push(e->pile, e->current[len], NULL)
+				: push(e->pile, e->current[len], "./");
 	clean(e->current);
 	if (e->pile->first == NULL)
 		push(e->pile, ".", NULL);
 	ft_putchar('\n');
-	return (i);
+	return (SUCCESS);
 }
 
 int				main(int ac, char **av)
